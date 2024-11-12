9161b15ae8b95773c3f99d9856e8412efe4d97f7
Redian Fikri <redianmf@gmail.com>
2024-11-12 14:42:53 +0700

    feat(listener): improve logic

--- src/config.ts
+++ src/config.ts
@@ -6,6 +6,7 @@ dotenv.config();
 export const ProviderURL = {
   ETHEREUM: process.env.MAINNET_PROVIDER_URL as string,
   ARBITRUM: process.env.ARBITRUM_PROVIDER_URL as string,
+  BINANCE: process.env.BSC_PROVIDER_URL as string,
 };
 
 const subGraphBaseUrl = `https://gateway.thegraph.com/api/${process.env.SUBGRAPH_API_KEY}/subgraphs/id/`;
@@ -24,8 +25,33 @@ export const WalletAddress = process.env.WALLET_ADDRESS as string;
 
 export const MainnetProviders = {
   alchemy: process.env.MAINNET_PROVIDER_URL,
+  alchemyWs: process.env.MAINNET_PROVIDER_URL_WS as string,
   chainnodes: process.env.CHAINNODES_PROVIDER_URL,
+  chainnodesWs: process.env.CHAINNODES_PROVIDER_URL_WS as string,
   chainstack: process.env.CHAINSTACK_PROVIDER_URL,
+  chainstackWs: process.env.CHAINSTACK_PROVIDER_URL_WS as string,
   infura: process.env.INFURA_PROVIDER_URL,
+  infuraWs: process.env.INFURA_PROVIDER_URL_WS as string,
   quickNode: process.env.QUICKNODE_PROVIDER_URL,
+  quickNodeWs: process.env.QUICKNODE_PROVIDER_URL_WS as string,
+  drpc: process.env.DRPC_PROVIDER_URL as string,
+  drpcWs: process.env.DRPC_PROVIDER_URL_WS as string,
+  blockPi: process.env.BLOCKPI_PROVIDER_URL as string,
+  blockPiWs: process.env.BLOCKPI_PROVIDER_URL_WS as string,
+  publicNode: process.env.PUBLICNODE_PROVIDER_URL as string,
+  publicNodeWs: process.env.PUBLICNODE_PROVIDER_URL_WS as string,
+  llama: process.env.LLAMA_PROVIDER_URL as string,
+  llamaWs: process.env.LLAMA_PROVIDER_URL_WS as string,
+  getBlockWs: process.env.GETBLOCK_PROVIDER_URL_WS as string,
+  omniaWs: process.env.OMNIA_PROVIDER_URL_WS as string,
+  nowNodesWs: process.env.NOWNODES_PROVIDER_URL_WS as string,
+  dwellirWs: process.env.DWELLIR_PROVIDER_URL_WS as string,
+  tenderlyWs: process.env.TENDERLY_PROVIDER_URL_WS as string,
+  blastWs: process.env.BLAST_PROVIDER_URL_WS as string,
+  callstaticWs: process.env.CALLSTATIC_PROVIDER_URL_WS as string,
+  cabinetWs: process.env.CABINET_PROVIDER_URL_WS as string,
+  kaleidoWs: process.env.KALEIDO_PROVIDER_URL_WS as string,
+  speedyNodesWs: process.env.SPEEDYNODES_PROVIDER_URL_WS as string,
+  merkleWs: process.env.MERKLE_PROVIDER_URL_WS as string,
+  zanWs: process.env.ZAN_PROVIDER_URL_WS as string,
 };
--- src/providers.ts
+++ src/providers.ts
@@ -1,44 +1,152 @@
 import { BigNumber, providers } from "ethers";
 import { MainnetProviders, WalletAddress } from "./config";
 
-async function main() {
-  const alchemyProvider = new providers.JsonRpcProvider(
-    MainnetProviders.alchemy
-  );
-  const chainnodesProvider = new providers.JsonRpcProvider(
-    MainnetProviders.chainnodes
-  );
-  const chainstackProvider = new providers.JsonRpcProvider(
-    MainnetProviders.chainstack
-  );
-  const infuraProvider = new providers.JsonRpcProvider(MainnetProviders.infura);
-  const quickNodeProvider = new providers.JsonRpcProvider(
-    MainnetProviders.quickNode
-  );
-
-  const promises = await Promise.all([
-    await measureTime(() => getBalance(alchemyProvider), "alchemy"),
-    await measureTime(() => getBalance(chainnodesProvider), "chainnodes"),
-    await measureTime(() => getBalance(chainstackProvider), "chainstack"),
-    await measureTime(() => getBalance(infuraProvider), "infura"),
-    await measureTime(() => getBalance(quickNodeProvider), "quicknode"),
-  ]);
-
-  console.log(promises);
+interface BenchmarkResponse {
+  providerName: string;
+  time: number;
+}
+interface BenchmarkResult {
+  providerName: string;
+  time: number[];
+  average: number;
+  averageWithoutFirstCall: number;
 }
 
-main().catch((err) => console.log(err));
+async function main() {
+  // const alchemyProvider = new providers.JsonRpcProvider(
+  //   MainnetProviders.alchemy
+  // );
+  // const chainnodesProvider = new providers.JsonRpcProvider(
+  //   MainnetProviders.chainnodes
+  // );
+  // const chainstackProvider = new providers.JsonRpcProvider(
+  //   MainnetProviders.chainstack
+  // );
+  // const infuraProvider = new providers.JsonRpcProvider(MainnetProviders.infura);
+  // const quickNodeProvider = new providers.JsonRpcProvider(
+  //   MainnetProviders.quickNode
+  // );
+  // const drpcProvider = new providers.JsonRpcProvider(MainnetProviders.drpc);
+  // const blockPiProvider = new providers.JsonRpcProvider(
+  //   MainnetProviders.blockPi
+  // );
+  // const publicNodeProvider = new providers.JsonRpcProvider(
+  //   MainnetProviders.publicNode
+  // );
+  // const llamaProvider = new providers.JsonRpcProvider(MainnetProviders.llama);
+
+  const alchemyProviderWs = new providers.WebSocketProvider(
+    MainnetProviders.alchemyWs
+  );
+  const chainnodesProviderWs = new providers.WebSocketProvider(
+    MainnetProviders.chainnodesWs
+  );
+  const chainstackProviderWs = new providers.WebSocketProvider(
+    MainnetProviders.chainstackWs
+  );
+  const infuraProviderWs = new providers.WebSocketProvider(
+    MainnetProviders.infuraWs
+  );
+  const quickNodeProviderWs = new providers.WebSocketProvider(
+    MainnetProviders.quickNodeWs
+  );
+  const drpcProviderWs = new providers.WebSocketProvider(
+    MainnetProviders.drpcWs
+  );
+  const blockPiProviderWs = new providers.WebSocketProvider(
+    MainnetProviders.blockPiWs
+  );
+  const publicNodeProviderWs = new providers.WebSocketProvider(
+    MainnetProviders.publicNodeWs
+  );
+  const llamaProviderWs = new providers.WebSocketProvider(
+    MainnetProviders.llamaWs
+  );
+  const getBlockProviderWs = new providers.WebSocketProvider(
+    MainnetProviders.getBlockWs
+  );
+  const omniaProviderWs = new providers.WebSocketProvider(
+    MainnetProviders.omniaWs
+  );
+
+  let benchmarkResult: BenchmarkResult[] = [];
+
+  for (let i = 0; i < 7; i++) {
+    const promises = await Promise.all([
+      // measureTime(() => getBalance(alchemyProvider), "alchemy"),
+      measureTime(() => getBalance(alchemyProviderWs), "alchemy ws"),
+      // measureTime(() => getBalance(chainnodesProvider), "chainnodes"),
+      measureTime(() => getBalance(chainnodesProviderWs), "chainnodes ws"),
+      // measureTime(() => getBalance(chainstackProvider), "chainstack"),
+      measureTime(() => getBalance(chainstackProviderWs), "chainstack ws"),
+      // measureTime(() => getBalance(infuraProvider), "infura"),
+      measureTime(() => getBalance(infuraProviderWs), "infura ws"),
+      // measureTime(() => getBalance(quickNodeProvider), "quicknode"),
+      measureTime(() => getBalance(quickNodeProviderWs), "quicknode ws"),
+      // measureTime(() => getBalance(drpcProvider), "drpc"),
+      measureTime(() => getBalance(drpcProviderWs), "drpc ws"),
+      // measureTime(() => getBalance(blockPiProvider), "blockPi"),
+      measureTime(() => getBalance(blockPiProviderWs), "blockPi ws"),
+      // measureTime(() => getBalance(publicNodeProvider), "publicNode"),
+      measureTime(() => getBalance(publicNodeProviderWs), "publicNode ws"),
+      // measureTime(() => getBalance(llamaProvider), "Llama"),
+      measureTime(() => getBalance(llamaProviderWs), "Llama ws"),
+      measureTime(() => getBalance(getBlockProviderWs), "getblock ws"),
+      measureTime(() => getBalance(omniaProviderWs), "omnia ws"),
+    ]);
+
+    if (i === 0) {
+      const transformedRes: BenchmarkResult[] = promises.map((item) => ({
+        providerName: item.providerName,
+        time: [item.time],
+        average: 0,
+        averageWithoutFirstCall: 0,
+      }));
+      benchmarkResult = transformedRes;
+    } else {
+      promises.forEach((response, idx) => {
+        benchmarkResult[idx].time.push(response.time);
+      });
+    }
+  }
+
+  const calculatedAverageTime = benchmarkResult
+    .map((result) => ({
+      ...result,
+      time: result.time.join(", "),
+      average:
+        result.time.reduce((acccumulator, time) => acccumulator + time, 0) /
+        result.time.length,
+      averageWithoutFirstCall:
+        result.time
+          .filter((_, idx) => idx !== 0)
+          .reduce((acccumulator, time) => acccumulator + time, 0) /
+        (result.time.length - 1),
+    }))
+    .sort((a, b) => a.averageWithoutFirstCall - b.averageWithoutFirstCall);
+
+  console.table(calculatedAverageTime);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((err) => console.log(err));
 
 async function measureTime(
   fn: () => Promise<BigNumber>,
   provider: string
-): Promise<string> {
+): Promise<BenchmarkResponse> {
   const start = performance.now();
   let elapsed = 0;
   await fn().then(() => {
     elapsed = performance.now() - start;
   });
-  return `Time with ${provider} = ${elapsed}`;
+
+  const response: BenchmarkResponse = {
+    providerName: provider,
+    time: Number(elapsed.toFixed(4)),
+  };
+  return response;
 }
 
 async function getBalance(
